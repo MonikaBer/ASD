3abbcf244ff4598d455a6500ea9f8e53f5db603c
Monika Berlińska <berlinska.monika@outlook.com>
2018-06-04 23:37:12 +0200

    Changes in tests.c in project of LIFO (stack)

--- LIFO/tests.c
+++ LIFO/tests.c
@@ -3,101 +3,77 @@
 #include <assert.h>
 #include "dec.h"
 
+struct el_LIFO* add_elements ( struct el_LIFO* top_LIFO, int first_added_element, int quantity_of_callings ){
+  int i = first_added_element;
+  quantity_of_callings += i;
+  while ( i < quantity_of_callings ){
+    top_LIFO = push ( top_LIFO, i );
+    assert ( top_LIFO->x == i );
+    i++;
+  }
+  return top_LIFO;
+}
 
-struct el_LIFO* create_LIFO ( struct el_LIFO *top_LIFO ) {
-  printf ( "\n\nDODAWANE ELEMENTY: 1,2,3,4,5,6,7\n" );
-  top_LIFO = push ( top_LIFO, 1 );
-  assert ( top_LIFO->x == 1 );
-  top_LIFO = push ( top_LIFO, 2 );
-  assert ( top_LIFO->x == 2 );
-  top_LIFO = push ( top_LIFO, 3 );
-  assert ( top_LIFO->x == 3 );
-  top_LIFO = push ( top_LIFO, 4 );
-  assert ( top_LIFO->x == 4 );
-  top_LIFO = push ( top_LIFO, 5 );
-  assert ( top_LIFO->x == 5 );
-  top_LIFO = push ( top_LIFO, 6 );
-  assert ( top_LIFO->x == 6 );
-  top_LIFO = push ( top_LIFO, 7 );
-  assert ( top_LIFO->x == 7 );
-
+struct el_LIFO* delete_elements ( struct el_LIFO* top_LIFO, int quantity_of_callings ){
+  int i = 0, value;
+  while ( i < quantity_of_callings ){
+    if ( top_LIFO != NULL )  value = top_LIFO->x;
+    top_LIFO = pop ( top_LIFO );
+    if ( top_LIFO != NULL )   assert ( top_LIFO->x == value-1 );
+    i++;
+  }
   return top_LIFO;
 }
 
 
-void test() {
+void test1() {
   struct el_LIFO *top_LIFO = NULL;
-  top_LIFO = create_LIFO ( top_LIFO );
-  top_LIFO = print_LIFO ( top_LIFO );
-  top_LIFO = print_LIFO ( top_LIFO );
-  top_LIFO = print_LIFO ( top_LIFO );
+  top_LIFO = add_elements ( top_LIFO, 1, 7 );
+  top_LIFO = delete_elements ( top_LIFO, 6 );
+  top_LIFO = add_elements ( top_LIFO, 2, 1 );
+  top_LIFO = delete_elements ( top_LIFO, 1 );
+  top_LIFO = add_elements ( top_LIFO, 2, 3 );
+  top_LIFO = delete_elements ( top_LIFO, 1 );
+  top_LIFO = add_elements ( top_LIFO, 4, 1 );
+  top_LIFO = empty_LIFO ( top_LIFO );
+  assert ( top_LIFO == NULL );
 
-  printf ( "\n\n" );
-  int i = 0;
-  while ( i < 6 ) {
-      top_LIFO = pop ( top_LIFO );
-      i++;
-  }
+  top_LIFO = add_elements ( top_LIFO, 1, 7 );
+  top_LIFO = delete_elements ( top_LIFO, 7 );
+  assert ( top_LIFO == NULL );
+  top_LIFO = delete_elements ( top_LIFO, 3 );
+  assert ( top_LIFO == NULL );
 
-  top_LIFO = print_LIFO ( top_LIFO );
+  top_LIFO = add_elements ( top_LIFO, 1, 1 );
   assert ( top_LIFO->x == 1 );
-  top_LIFO = push ( top_LIFO, 1 );
-  assert ( top_LIFO->x == 1 );
-  top_LIFO = pop ( top_LIFO );
-  assert ( top_LIFO->x == 1 );
-  top_LIFO = push ( top_LIFO, 2 );
-  assert ( top_LIFO->x == 2 );
-  top_LIFO = push ( top_LIFO, 5 );
-  assert ( top_LIFO->x == 5 );
-  top_LIFO = push ( top_LIFO, 4 );
-  assert ( top_LIFO->x == 4 );
-  top_LIFO = pop ( top_LIFO );
-  assert ( top_LIFO->x == 5 );
-  top_LIFO = push ( top_LIFO, 8 );
-  assert ( top_LIFO->x == 8 );
-  top_LIFO = print_LIFO ( top_LIFO );
-  top_LIFO = print_LIFO ( top_LIFO );
-  top_LIFO = print_LIFO ( top_LIFO );
 
   top_LIFO = empty_LIFO ( top_LIFO );
-  top_LIFO = print_LIFO ( top_LIFO );
-  top_LIFO = print_LIFO ( top_LIFO );
-
-  top_LIFO = push ( top_LIFO, 1 );
-  assert ( top_LIFO->x == 1 );
-  top_LIFO = push ( top_LIFO, 2 );
-  assert ( top_LIFO->x == 2 );
-  top_LIFO = push ( top_LIFO, 3 );
-  assert ( top_LIFO->x == 3 );
-  top_LIFO = push ( top_LIFO, 4 );
-  assert ( top_LIFO->x == 4 );
-  top_LIFO = push ( top_LIFO, 5 );
-  assert ( top_LIFO->x == 5 );
-  top_LIFO = push ( top_LIFO, 6 );
-  assert ( top_LIFO->x == 6 );
-  top_LIFO = push ( top_LIFO, 7 );
-  assert ( top_LIFO->x == 7 );
-
-  i = 0;
-  while ( i < 7 ) {
-      top_LIFO = pop ( top_LIFO );
-      i++;
-  }
   assert ( top_LIFO == NULL );
-  top_LIFO = pop ( top_LIFO );
-  top_LIFO = pop ( top_LIFO );
-  top_LIFO = pop ( top_LIFO );
+}
+
+void test2(){
+  struct el_LIFO *top_LIFO = NULL;
+  top_LIFO = empty_LIFO ( top_LIFO );
   assert ( top_LIFO == NULL );
-  top_LIFO = push ( top_LIFO, 5 );
-  assert ( top_LIFO->x == 5 );
+
+  top_LIFO = add_elements ( top_LIFO, 1, 7 );
+  top_LIFO = delete_elements ( top_LIFO, 6 );
+  top_LIFO = add_elements ( top_LIFO, 2, 1 );
+  top_LIFO = delete_elements ( top_LIFO, 1 );
+  top_LIFO = add_elements ( top_LIFO, 2, 3 );
+  top_LIFO = delete_elements ( top_LIFO, 1 );
+  top_LIFO = add_elements ( top_LIFO, 4, 1 );
 
   top_LIFO = empty_LIFO ( top_LIFO );
+  assert ( top_LIFO == NULL );
 }
 
 
 int main(){
-  printf ( "\nTEST\n\n" );
-  test();
+  printf ( "\nTEST_1\n\n" );
+  test1();
+  printf ( "\nTEST_2\n\n" );
+  test2();
 
   return 0;
 }
